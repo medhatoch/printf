a2ad6cd6828f85e3d351523405babc1fbba5b641
Ayoub EL AKKAL <akkalayoub3@gmail.com>
2023-04-25 18:06:45 -0700

    ayoub

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 /**
  * _printf - produces output according to a format
@@ -11,69 +10,50 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, j = 0, len = 0;
-    char buf[1024] = {0};
-    int buf_len = 0;
+	va_list args;
+	int i = 0, j = 0, len = 0;
 
-    format_t formats[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_percent},
-        {"i", print_integer},
-        {"d", print_decimal},
-        {"b", print_binary},
-        {"u", print_unsigned_integer},
-        {"o", print_octal},
-        {"x", print_hex_lowercase},
-        {"X", print_hex_uppercase},
-        {NULL, NULL}
-    };
+	format_t formats[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"i", print_integer},
+		{"d", print_decimal},
+		{"b", print_binary},
+		{NULL, NULL}
+	};
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                return (-1);
-            for (j = 0; formats[j].fmt != NULL; j++)
-            {
-                if (formats[j].fmt[0] == *format)
-                {
-                    int n = formats[j].f(args);
-                    if (n < 0)
-                        return (-1);
-                    buf_len += n;
-                    break;
-                }
-            }
-            if (formats[j].fmt == NULL)
-            {
-                if (format[i] != ' ')
-                    buf_len += _putchar_buf(buf + buf_len, sizeof(buf) - buf_len, '%');
-                buf_len += _putchar_buf(buf + buf_len, sizeof(buf) - buf_len, format[i]);
-            }
-        }
-        else
-        {
-            buf_len += _putchar_buf(buf + buf_len, sizeof(buf) - buf_len, format[i]);
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+			for (j = 0; formats[j].fmt != NULL; j++)
+			{
+				if (*(formats[j].fmt) == format[i])
+				{
+					len += formats[j].f(args);
+					break;
+				}
+			}
+			if (formats[j].fmt == NULL)
+			{
+				if (format[i] != ' ')
+					len += _putchar('%');
+				len += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			len += _putchar(format[i]);
+		}
+		i++;
+	}
 
-    va_end(args);
-
-    /* write any remaining characters to stdout */
-    if (buf_len > 0)
-    {
-        len = write(1, buf, buf_len);
-        if (len < 0)
-            return (-1);
-    }
-
-    return (buf_len);
+	va_end(args);
+	return (len);
 }
-
