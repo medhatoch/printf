50261e04c6b7e3f8eca029cfe2500af6fed25e5b
Ayoub EL AKKAL <akkalayoub3@gmail.com>
2023-04-26 02:17:21 +0000

    removed spaces

--- _printf.c
+++ _printf.c
@@ -10,54 +10,54 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, j = 0, len = 0;
+	va_list args;
+	int i = 0, j = 0, len = 0;
 
-    format_t formats[] = {
-        {"c", print_char},
-        {"s", print_string},
-        {"%", print_percent},
-        {"i", print_integer},
-        {"d", print_decimal},
+	format_t formats[] = {
+	{"c", print_char},
+	{"s", print_string},
+	{"%", print_percent},
+	{"i", print_integer},
+	{"d", print_decimal},
 	{"b", print_binary},
 	{"u", print_unsigned_integer},
-        {"o", print_octal},
-        {"x", print_hex_lowercase},
-        {"X", print_hex_uppercase},
-        {NULL, NULL}
-    };
+	{"o", print_octal},
+	{"x", print_hex_lowercase},
+	{"X", print_hex_uppercase},
+	{NULL, NULL}
+	};
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                return (-1);
-            for (j = 0; formats[j].fmt != NULL; j++)
-            {
-                if (*(formats[j].fmt) == format[i])
-                {
-                    len += formats[j].f(args);
-                    break;
-                }
-            }
-            if (formats[j].fmt == NULL)
-            {
-                if (format[i] != ' ')
-                    len += _putchar('%');
-                len += _putchar(format[i]);
-            }
-        }
-        else
-        {
-            len += _putchar(format[i]);
-        }
-        i++;
-    }
+	while (format && format[i])
+	{
+	if (format[i] == '%')
+	{
+		i++;
+		if (format[i] == '\0')
+			return (-1);
+		for (j = 0; formats[j].fmt != NULL; j++)
+		{
+			if (*(formats[j].fmt) == format[i])
+			{
+				len += formats[j].f(args);
+				break;
+			}
+		}
+		if (formats[j].fmt == NULL)
+		{
+			if (format[i] != ' ')
+				len += _putchar('%');
+			len += _putchar(format[i]);
+		}
+	}
+	else
+	{
+		len += _putchar(format[i]);
+	}
+	i++;
+	}
 
-    va_end(args);
-    return (len);
+	va_end(args);
+	return (len);
 }
